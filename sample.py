a93edbe1ed9fec7756f51733eefa26192535a0b1
Ashutosh Sharma <45865775+spider0061@users.noreply.github.com>
2019-12-05 19:23:16 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,13 @@
+from Cars import * 
+   
+# Create an object of Bmw class & call its method 
+ModBMW = Bmw() 
+ModBMW.outModels() 
+   
+# Create an object of Audi class & call its method 
+ModAudi = Audi() 
+ModAudi.outModels() 
+  
+# Create an object of Nissan class & call its method 
+ModNissan = Nissan() 
+ModNissan.outModels() 
